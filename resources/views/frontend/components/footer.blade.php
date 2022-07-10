e9c125174c4df171fc621761d1956e17c535d71d
amit <amit2054128@gmail.com>
2022-07-10 12:01:18 +0545

    changed

--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -3,8 +3,8 @@
         <div class="row g-5">
             <div class="col-lg-4 col-md-6">
                 <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Company</h4>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">ankit</a>
+                <a class="btn btn-link" href="">ankit</a>
                 <a class="btn btn-link" href="">Privacy Policy</a>
                 <a class="btn btn-link" href="">Terms & Condition</a>
             </div>
